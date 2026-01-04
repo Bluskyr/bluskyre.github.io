007c8405bddbd17163973f8c351ae8b00a1da222
Bluskyr <ecakilci6@gmail.com>
2026-01-04 14:37:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "linkler-v1";
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache =>
+            cache.addAll([
+                "/",
+                "/index.html",
+                "/avatar.jpg"
+            ])
+        )
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
