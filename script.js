5bda58a4850adfff693c0b4d4256e9888f3779b5
Bluskyr <ecakilci6@gmail.com>
2026-01-17 23:58:50 +0300

    Refactor view counter and add title animation

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// 1. BAŞLIK ANİMASYONU
+const titleText = "BLUSKYRE";
+let charIndex = 0;
+let isDeleting = false;
+
+function animateTitle() {
+    const currentTitle = titleText.substring(0, charIndex);
+    document.title = currentTitle || "|";
+
+    if (!isDeleting && charIndex < titleText.length) {
+        charIndex++;
+        setTimeout(animateTitle, 300);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(animateTitle, 150);
+    } else {
+        isDeleting = !isDeleting;
+        setTimeout(animateTitle, 2000);
+    }
+}
+animateTitle();
+
+// 2. GLOBAL GÖRÜNTÜLENME SAYACI
 async function updateViews() {
     const countEl = document.getElementById('view-count');
     try {
@@ -13,4 +36,15 @@ async function updateViews() {
         countEl.innerText = localV + " Görüntülenme";
     }
 }
-updateViews()
+updateViews();
+
+// 3. TEMA DEĞİŞTİRME
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.getElementById('theme-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.className = 'fas fa-sun';
+    } else {
+        icon.className = 'fas fa-moon';
+    }
+}
